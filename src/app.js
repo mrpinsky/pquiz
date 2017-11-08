02604d2d65eb5a09a8fd4c2f02baaed4025e3662
Nate Pinsky <n.j.pinsky@gmail.com>
2017-11-07 22:33:27 -0800

    Separate dev and prod builds

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 require('./index.html');
 require('./styles/styles.scss');
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,21 @@
 var path = require("path");
-
+var webpack = require("webpack");
+var merge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
+const prod = 'production';
+const dev = 'development';
+
+// determine build env
+const TARGET_ENV = process.env.npm_lifecycle_event === 'build' ? prod : dev;
+const isDev = TARGET_ENV == dev;
+const isProd = TARGET_ENV == prod;
+
+console.log("WEBPACK STARTED")
+console.log("Building for", TARGET_ENV)
+
+var commonConfig = {
   entry: {
     app: './src/app.js',
   },
@@ -17,14 +30,7 @@ module.exports = {
         test: /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
         loader: 'elm-webpack-loader?verbose=true&warn=true',
-        options: {
-          debug: true,
-        },
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
+      }
     ],
     noParse: /\.elm$/,
   },
@@ -39,3 +45,58 @@ module.exports = {
     filename: '[name].bundle.js',
   },
 };
+
+if (isDev) {
+  module.exports = merge(commonConfig, {
+    module: {
+      rules: [{
+        test: /\.elm$/,
+        exclude: [/elm-suff/, /node_modules/],
+        use: [{
+          loader: 'elm-webpack-loader',
+          options: {
+            verbose: true,
+            warn: true,
+            debug: true
+          }
+        }]
+      },{
+        test: /\.sc?ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }]
+    }
+  });
+}
+
+if (isProd) {
+  module.exports = merge(commonConfig, {
+    module: {
+      rules: [{
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: 'elm-webpack-loader',
+      }, {
+        test: /\.sc?ss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }]
+    },
+    plugins: [
+      new ExtractTextPlugin({
+        filename: 'styles.css',
+      }),
+
+      // extract CSS into a separate file
+      // minify JS
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        compressor: {
+          warnings: false
+        }
+      })
+    ]
+  });
+}
+
